f678f5a398d52ffe246431a653401b9d88f6bfcc
Kevin Schwarz <Kevin Schwarz>
2016-07-04 18:02:35 -0400

    angularized hello world

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'running',
+  template: '<h1>Running of the Wolves</h1><img src="/images/pawprint.jpg" alt="Liberty Pines Running of the Wolves"/>'
+})
+export class AppComponent { }
