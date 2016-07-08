fbb8df991c12f78691b6eddf4728f3ddfb224c53
Kevin Schwarz <Kevin@k-schwarz.com>
2016-07-07 22:11:51 -0400

    some lpa quick and dirty

--- client/components/app.component.ts
+++ client/components/app.component.ts
@@ -5,14 +5,12 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
     directives: [ROUTER_DIRECTIVES],
     selector: "app",
     template: `<div class="ui container">
-        <div class="center">
-            <img src='https://angular.io/resources/images/logos/standard/shield-large.png'>
-        </div>
-        <router-outlet></router-outlet>
-        <nav class="ui menu inverted teal">
+        <br />
+        <nav class="ui menu inverted blue" >
             <a [routerLink]="['/']" class="item">Home</a>
-            <a [routerLink]="['/contact']" class="item">Contact Me</a>
+            <a [routerLink]="['/contact']" class="item">Contact</a>
         </nav>
+        <router-outlet></router-outlet>        
       </div>`
 })
 export class AppComponent {}
