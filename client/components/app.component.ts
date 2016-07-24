ad6babdb5da58010c5f5a79027978c06e3960bd5
Kevin Schwarz <Kevin@k-schwarz.com>
2016-07-24 10:47:13 -0400

    fixed navbar, added sponsorship sheet, added coursemap, cleanup formatting

--- client/components/app.component.ts
+++ client/components/app.component.ts
@@ -19,26 +19,28 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
         <nav class="navbar navbar-default" role="navigation">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"aria-controls="navbar">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
                 </div>
-                <ul class="nav navbar-nav">
-                    <!--<li class="active"><a [routerLink]="['/event_information']">Event Information <span class="sr-only">(current)</span></a></li>-->
-                    <li><a [routerLink]="['/']">Home</a></li>
-                    <li><a [routerLink]="['/event_information']">Event Information</a></li>
-                    <li><a [routerLink]="['/course']">Course</a></li>
-                    <li><a [routerLink]="['/photos']">Photos</a></li>
-                    <li><a [routerLink]="['/results']">Results</a></li>
-                    <li><a [routerLink]="['/sponsors']">Sponsors</a></li>
-                    <li><a [routerLink]="['/contact']">Contact</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a [routerLink]="['/pto']">PTO </a> </li> 
-                </ul>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <!--<li class="active"><a [routerLink]="['/event_information']">Event Information <span class="sr-only">(current)</span></a></li>-->
+                        <li><a [routerLink]="['/']">Home</a></li>
+                        <li><a [routerLink]="['/event_information']">Event Information</a></li>
+                        <li><a [routerLink]="['/course']">Course</a></li>
+                        <li><a [routerLink]="['/photos']">Photos</a></li>
+                        <li><a [routerLink]="['/results']">Results</a></li>
+                        <li><a [routerLink]="['/sponsors']">Sponsors</a></li>
+                        <li><a [routerLink]="['/contact']">Contact</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a [routerLink]="['/pto']">PTO </a> </li> 
+                    </ul>
+                </div>
             </div>
         </nav>
         <router-outlet></router-outlet>        
